d008d5bf6a45e77bb8fca023ac7dd6997dc22417
Remi <rbhagalou@citadia.com>
2019-10-08 07:57:01 +0200

    ajout de la fonction name

--- script.py
+++ script.py
@@ -2,3 +2,7 @@ def helloWorld:
     while question != y:
         question = input('Are you ok? y/n')
     print('Hello world')
+
+def name:
+    name = input("What's your name?")
+    print('Hello ' + name)
